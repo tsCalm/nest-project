7e732de582d01d73d8476af32ae79cba1c5d2cbf
tsCalm <dlatkdals93@gmail.com>
2023-05-07 18:26:32 +0900

    feat: ProjectConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProjectConfigModule } from './modules/config';
 
 @Module({
-  imports: [],
+  imports: [ProjectConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
